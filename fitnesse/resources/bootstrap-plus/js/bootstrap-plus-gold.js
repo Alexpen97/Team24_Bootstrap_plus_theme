e6f5628fb88e0de29ac2038a82c1e9b7c8589142
alex pennings <apennings@roc-dev.com>
2019-12-11 21:34:05 +0100

    Tooltip function from csv file, documentation pending.

---
+++ fitnesse/resources/bootstrap-plus/js/bootstrap-plus-gold.js
@@ -0,0 +1,19 @@
+var hello = "hello";
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "files/fitnesse/bootstrap-plus/csv/toolTipData.csv",
+        dataType: "text",
+        success: function(data) {displayToolTip(data);}
+    });
+});
+
+function displayToolTip(text){
+var textarray = text.split(",");
+var PickedTip = Math.floor(Math.random() * textarray.length);
+var footer = document.querySelector('footer');
+var tooltip = document.createElement("p");
+footer.appendChild(tooltip);
+tooltip.id = "tooltip";
+tooltip.innerHTML =  "<img src='/files/fitnesse/bootstrap-plus/img/hint.png'>" + textarray[PickedTip];
+}
